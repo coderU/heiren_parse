e4f3eed20e95108ea270790470615972faa01836
coderU <jemit.uriel@gmail.com>
2015-08-01 23:39:59 +0800

    login and register

--- cloud/app.js
+++ cloud/app.js
@@ -1,4 +1,6 @@
 User = require("cloud/users.js")
+parseExpressHttpsRedirect = require('parse-express-https-redirect');
+parseExpressCookieSession = require('parse-express-cookie-session');
 // These two lines are required to initialize Express in Cloud Code.
 express = require('express');
 app = express();
@@ -7,17 +9,45 @@ app = express();
 app.set('views', 'cloud/views');  // Specify the folder to find templates
 app.set('view engine', 'ejs');    // Set the template engine
 app.use(express.bodyParser());    // Middleware for reading request body
-
+//app.use(parseExpressHttpsRedirect());  // Require user to be on HTTPS.
+app.use(express.cookieParser('YOUR_SIGNING_SECRET'));
+app.use(parseExpressCookieSession({ cookie: { maxAge: 3600000 } }));
 // This is an example of hooking up a request handler with a specific request
 // path and HTTP verb using the Express routing API.
 app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
 
-app.post('/hello', function(req, res) {
-    User.register(req.body.message, res);
+
+//Apis associate with users
+app.post('/apis/users', function(req, res) {
+    User.register(req.body.email, req.body.password, req.body.fullname, req.body.email, req.body.phone, function(result){
+      if(result != "ok"){
+        res.status(500).send(result);
+      }else{
+        res.send(result);
+      }
+    });
 });
 
+app.get('/apis/users', function(req, res) {
+    User.login(req.query.email, req.query.password, function(result){
+      if(result != "ok"){
+        res.status(500).send(result);
+      }else {
+        res.send(result);
+      }
+    });
+});
+
+app.get('/apis/users/me', function(req, res) {
+  var currentUser = Parse.User.current();
+  if (currentUser) {
+      res.send(currentUser);
+  } else {
+      res.send("Login page");
+  }
+});
 
 
 // // Example reading from the request query string of an HTTP get request.
--- cloud/users.js
+++ cloud/users.js
@@ -1,7 +1,37 @@
 var User={
-    register:function(msg, res){
-	res.render(msg);
+    register:function(username, password, fullname, email, phone, callback){
+      var user = new Parse.User();
+      user.set("username", username);
+      user.set("password", password);
+      user.set("email", email);
+
+      // other fields can be set just like with Parse.Object
+      user.set("phone", phone);
+      user.set("fullname", fullname);
+      user.signUp(null, {
+        success: function(user) {
+          callback("ok");
+        },
+        error: function(user, error) {
+          // Show the error message somewhere and let the user try again.
+          alert("Error: " + error.code + " " + error.message);
+          callback("Error: " + error.message);
+        }
+      });
+    },
+
+    login:function(username, password, callback){
+      console.log("login with: "+username+", "+password)
+      Parse.User.logIn(username, password, {
+        success: function(user) {
+          callback("ok")
+        },
+        error: function(user, error) {
+          callback("Error: " + error.message);
+        }
+      });
     }
+
 }
 
 module.exports  = User;
